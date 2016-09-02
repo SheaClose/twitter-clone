2a02afec3d9cf424c2fa3b52558054980bfc9e7f
SheaClose <SheaClose@gmail.com>
2016-09-02 14:42:38 -0500

    submitted tweets now show up in the streamgit add -A

--- js/animations.js
+++ js/animations.js
@@ -31,8 +31,21 @@ $(document).ready(function () {
 
   $("#tweet-submit").on('click', function() {
     var tweetstr = $('.tweet-compose').val()
-      $('#stream').prepend(tweetstr);
-      console.log(tweetstr);
+      $('#stream').prepend(
+'<div class="tweet"><div class="content"><img class="avatar" src="img/alagoon.jpg" />' +
+'<strong class="fullname">Shea Close</strong><span class="username">@Shea_Close</span>'+
+'<p class="tweet-text">'+tweetstr+'</p><div class="tweet-actions"><ul>'+
+'<li><span class="icon action-reply"></span> Reply</li><li>'+
+'<span class="icon action-retweet">' +
+'</span> Retweet</li><li><span class="icon action-favorite"></span> Favorite</li>'+
+'<li><span class="icon action-more"></span> More</li> </ul> </div> <div class="stats"> '+
+'<div class="retweets"><p class="num-retweets">30</p><p>RETWEETS</p></div>'+
+'<div class="favorites"><p class="num-favorites">6</p><p>FAVORITES</p></div>'+
+'<div class="users-interact"><div><img src="img/alagoon.jpg" />'+
+'<img src="img/vklimenko.jpg" /></div></div><div class="time">1:04 PM - 19 Sep 13</div>'+
+'</div><div class="reply"><img class="avatar" src="img/alagoon.jpg" />'+
+'<textarea class="tweet-compose" placeholder="Reply to  @mybff"/>' +
+'</textarea></div></div></div>');
     });
 
 
